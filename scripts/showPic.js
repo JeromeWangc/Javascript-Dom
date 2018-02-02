265237d3a2cd79a55f30cdefe1c9c0e2937fb130
JeromeWang <mulituzhen@gmail.com>
2018-02-02 14:15:31 +0800

    dymanic create placeholder

--- scripts/showPic.js
+++ scripts/showPic.js
@@ -1,13 +1,51 @@
+function addLoadEvent(func){
+    var oldonload = window.onload;
+    if (typeof window.onload != 'function') {
+        window.onload = func;
+    }else {
+        window.onload = function () {
+            oldonload();
+            func();
+        }
+    }
+}
+
+function insertAfter(newElement,targetElement) {
+    var parent = targetElement.parentNode;
+    if (parent.lastChild == targetElement) {
+        parent.appendChild(newElement);
+    }else {
+        parent.insertBefore(newElement,targetElement.nextSibling);
+    }
+}
+
+function preparePlaceholder() {
+    if (!document.createElement) return false;
+    if (!document.createTextNode) return false;
+    if (!document.getElementById) return false;
+    if (!document.getElementById("imagegallery")) return false;
+    var placeholder = document.createElement("img");
+    placeholder.setAttribute("id","placeholder");
+    placeholder.setAttribute("src","images/meal.jpg");
+    placeholder.setAttribute("alt","my image gallery");
+    var description = document.createElement("p");
+    description.setAttribute("id","description");
+    var desctext = document.createTextNode("Choose an image");
+    description.appendChild(desctext);
+    var gallery = document.getElementById("imagegallery");
+    insertAfter(placeholder,gallery);
+    insertAfter(description,placeholder);
+}
+
 function showPic(whichpic) {
     if (!document.getElementById("placeholder")) return false;
     var source = whichpic.getAttribute("href");
     var placeholder = document.getElementById("placeholder");
     placeholder.setAttribute("src",source);
-    if (document.getElementById("description")) {
-        var text = whichpic.getAttribute("title") ? whichpic.getAttribute("title") : "";
-        var description = document.getElementById("description");
-        description.firstChild.nodeValue = text;
-    }
+    if (!document.getElementById("description")) return false;
+    var text = whichpic.getAttribute("title") ? whichpic.getAttribute("title") : "";
+    var description = document.getElementById("description");
+    description.firstChild.nodeValue = text;
     return true;
 }
 
@@ -24,4 +62,10 @@ function prepareGallery() {
     }
 }
 
-window.onload = prepareGallery;
+/*window.onload = function (){
+    preparePlaceholder();
+    prepareGallery();
+}*/
+
+addLoadEvent(preparePlaceholder);
+addLoadEvent(prepareGallery);
